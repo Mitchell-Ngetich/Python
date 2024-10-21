17139a37ba49efe776bb242cdb1cdeb7c616a452
Mithell Ngetich <mitchell.ngetich@student.moringaschool.com>
2024-10-21 13:33:11 +0300

    Initial commit

---
+++ Analytics/script.py
@@ -0,0 +1,13 @@
+print(2+2)
+print(3 * 3)
+
+salary = 10000
+print(salary)
+
+company_name = "Hapag-Lloyd shipping company"
+print(company_name)
+job_wfh = True
+print(job_wfh)
+print(type(salary))
+print(type(company_name))
+print(type(job_wfh))
