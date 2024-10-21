812e9257db45e4e31264e2f59eff07f9dab0b702
Mithell Ngetich <mitchell.ngetich@student.moringaschool.com>
2024-10-21 23:03:59 +0300

    functions

--- Analytics/script.py
+++ Analytics/script.py
@@ -1,13 +1,37 @@
-print(2+2)
-print(3 * 3)
+# print(2+2)
+# print(3 * 3)
 
-salary = 10000
-print(salary)
+# salary = 10000
+# print(salary)
 
-company_name = "Hapag-Lloyd shipping company"
-print(company_name)
-job_wfh = True
-print(job_wfh)
-print(type(salary))
-print(type(company_name))
-print(type(job_wfh))
+# company_name = "Hapag-Lloyd shipping company"
+# print(company_name)
+# job_wfh = True
+# print(job_wfh)
+# print(type(salary))
+# print(type(company_name))
+# print(type(job_wfh))
+
+#functions
+# def greetings():
+#     return "how are you Ser?"
+# print(greetings()) */
+
+# def happy_birthday(name, age):
+#     print(f"Happy birthday to {name}!")
+#     print(f"How old are you!, {age}")
+#     print(f"Happy birthday dear {name}!")
+# happy_birthday("Ser", 25)
+# happy_birthday("Joy", 9)
+# happy_birthday("Pepe", 9)
+
+# def add(a,b):
+#     return a * b
+# print(add(5,9))
+
+def full_name(first_name, last_name):
+    first = first_name.capitalize()
+    last = last_name.capitalize()
+    final_name = first + " " + last
+    # return final_name
+print(full_name("ser","mitch"))
