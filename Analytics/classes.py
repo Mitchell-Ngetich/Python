fd15405ce48fcdad08bc94c13cb61301fab1c7e4
Mitchell Ngetich <mitchell.ngetich@student.moringaschool.com>
2024-12-16 17:11:30 +0300

    Classes

--- Analytics/classes.py
+++ Analytics/classes.py
@@ -1,18 +1,29 @@
-class Employee:
-    def __init__(self, first, last, salary):
-        self.first =  first
-        self.last =  last
-        self.salary =  salary
-        self.email = first + "." + last + "@company.com"
+# class Employee:
+#     def __init__(self, first, last, salary):
+#         self.first =  first
+#         self.last =  last
+#         self.salary =  salary
+#         self.email = first + "." + last + "@company.com"
     
-    def fullname(self):
-        return f"{self.first} {self.last}" #always use two braces
+#     def fullname(self):
+#         return f"{self.first} {self.last}" #always use two braces
     
-"""below will print the same thing. when we call the method on a class we have to pass in the instance as
-an argument, but we can call the instance by itself then add self as an argument to the class method"""
-emp_1 = Employee("Mitchell", "Ngetich", 5000)
+# """below will print the same thing. when we call the method on a class we have to pass in the instance as
+# an argument, but we can call the instance by itself then add self as an argument to the class method"""
+# emp_1 = Employee("Mitchell", "Ngetich", 5000)
 # print(Employee.fullname(emp_1)) """this is calling a method on the class. Less common as you have to pass in 
 # instance as a variable"""
 
-print(emp_1.fullname())
+# print(emp_1.fullname())
+
+class Salary:
+    def __init__(self, base_salary,bonus):
+        self.base_salary = base_salary
+        self.bonus = bonus
+
+    def Total_salary(self):
+        return f"{self.base_salary + self.bonus}"
+
+emp_salary = Salary(50000, 20000)
+print(emp_salary.Total_salary())
 
