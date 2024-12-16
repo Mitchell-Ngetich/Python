42be0902e1f353820f973a3ef3c8bef1421af433
Mitchell Ngetich <mitchell.ngetich@student.moringaschool.com>
2024-12-16 16:41:10 +0300

    classes

--- Analytics/classes.py
+++ Analytics/classes.py
@@ -8,8 +8,11 @@ class Employee:
     def fullname(self):
         return f"{self.first} {self.last}" #always use two braces
     
+"""below will print the same thing. when we call the method on a class we have to pass in the instance as
+an argument, but we can call the instance by itself then add self as an argument to the class method"""
 emp_1 = Employee("Mitchell", "Ngetich", 5000)
-print(Employee.fullname(emp_1))
+# print(Employee.fullname(emp_1)) """this is calling a method on the class. Less common as you have to pass in 
+# instance as a variable"""
 
 print(emp_1.fullname())
 
